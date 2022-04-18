c6d6c8dba5e67beeffcce594d9b1e27b0f78986e
thushAR13 <thusharkulal@gmail.com>
2022-04-18 22:42:47 +0530

    Nav bar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'movies#index'
+  get 'movies' => 'movies#index'
   get 'movies/higross'
 
   # Defines the root path route ("/")
